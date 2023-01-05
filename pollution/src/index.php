51701e0bb1ddcfe84e926385959c0fdf6adca13a
akhils911dev <dev.akhils911@gmail.com>
2023-01-05 10:49:30 +0530

    adding more files

---
+++ pollution/src/index.php
@@ -0,0 +1,22 @@
+<?php
+
+require '../bootstrap.php';
+
+use app\classes\Routes;
+use app\classes\Uri;
+
+
+$routes = [
+    "/" => "controllers/index.php",
+    "/login" => "controllers/login.php",
+    "/register" => "controllers/register.php",
+    "/home" => "controllers/home.php",
+    "/admin" => "controllers/admin.php",
+    "/api" => "controllers/api.php",
+    "/set/role/admin" => "controllers/set_role_admin.php",
+    "/logout" => "controllers/logout.php"
+];
+
+$uri = Uri::load();
+require Routes::load($uri, $routes);
+
