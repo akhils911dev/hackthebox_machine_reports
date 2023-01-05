1fe8b6e0e5c07a8562c3eb1f457d753b18f0d488
akhils911dev <dev.akhils911@gmail.com>
2023-01-05 10:56:11 +0530

    src has been removed

--- pollution/src/bootstarp.php
+++
@@ -1,8 +0,0 @@
-<?php
-ini_set('session.save_handler','redis');
-ini_set('session.save_path','tcp://127.0.0.1:6379/?auth=COLLECTR3D1SPASS');
-
-session_start();
-
-require '../vendor/autoload.php';
-
